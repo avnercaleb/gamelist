c99fae360abceab02e3bb2cd7a6815e1a5839c77
avnercaleb <avnercaleb@hotmail.com>
2023-05-10 20:02:26 -0300

    Seed do banco de dados de teste

--- src/main/java/com/midasvision/gamelist/entities/Game.java
+++ src/main/java/com/midasvision/gamelist/entities/Game.java
@@ -5,18 +5,24 @@ import lombok.*;
 
 @Entity
 @Data
+@Table(name = "tb_game")
 public class Game {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
     @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 }
